f5d0d8fff60bc991a8c30a18a0d3fb289a20e6ac
Patrick Hurst <phurst@amateurtopologist.com>
2011-06-25 02:02:44 -0400

    add a function to handle incoming tags, along with an example

--- autotagger.js
+++ autotagger.js
@@ -1,10 +1,11 @@
-input = $('#tag_editor_input')[0];
-tokens = $('#tokens')[0];
+var input = $('#tag_editor_input')[0];
+var tokens = $('#tokens')[0];
 
 function add_tag(tag) {
-    tok = document.getElementById("tokens");
-    console.log(tok.constructor.toString());
-    tok.innerHTML += '<div class="token"><span class="tag">' + tag + '</span><a href="#" onclick="tag_editor_remove_tag($(this).up()); return false;">x</a></div>';
+    if (get_tags().indexOf(tag) != -1) // no dupe tags
+	return;
+
+    tokens.innerHTML += '<div class="token"><span class="tag">' + tag + '</span><a href="#" onclick="tag_editor_remove_tag($(this).up()); return false;">x</a></div>';
 }
 
 function get_tags() {
@@ -12,3 +13,20 @@ function get_tags() {
     $('.token > .tag').each(function() { tagTexts.push($(this).text()) } );
     return tagTexts;
 }
+
+
+
+function handleNewTag(event) {
+    var newTag = event.target.children[0].innerHTML;
+    if (newTag in triggers) {
+	tokens.removeEventListener("DOMNodeInserted", handleNewTag, false);
+	for (tag in triggers[newTag]) {
+	    add_tag(triggers[newTag][tag]);
+	}
+    }
+    tokens.addEventListener("DOMNodeInserted", handleNewTag, false);
+}
+
+tokens.addEventListener("DOMNodeInserted", handleNewTag, false);
+
+var triggers = {"COOLK1DS B31NG COOL": ["dave", "terezi"]};
